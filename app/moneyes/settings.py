5d899e6c3f3f239631fd28680679c50902c055e4
tom-guastapaglia <tomguasta@yahoo.fr>
2022-02-13 16:50:56 +0100

    settings

--- app/moneyes/settings.py
+++ app/moneyes/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", False)
 
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS")
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
 # Application definition
 
